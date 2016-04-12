d0fb5399cd14d4a7290f9d14b3715f48355b3d47
Mateusz Jaje <mateuszjaje@gmail.com>
2016-04-12 21:55:14 +0200

    added heroku support

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-var webpack = require('webpack')
-var webpackDevMiddleware = require('webpack-dev-middleware')
-var webpackHotMiddleware = require('webpack-hot-middleware')
-var config = require('./webpack.config')
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
+var config = require('./webpack.config');
 
 var express = require('express');
-var app = new (express)()
-var port = 3000
+var app = new (express)();
+var port = process.env.PORT || 5000;
 
-var compiler = webpack(config)
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
-app.use(webpackHotMiddleware(compiler))
+
+var compiler = webpack(config);
+app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath}));
+app.use(webpackHotMiddleware(compiler));
 app.use('/scripts', express.static(__dirname + '/node_modules/react-widgets/dist/'));
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + '/index.html')
-})
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + '/index.html')
+});
 
-app.listen(port, function(error) {
-  if (error) {
-    console.error(error)
-  } else {
-    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
-  }
-})
+app.listen(port, function (error) {
+    if (error) {
+        console.error(error)
+    } else {
+        console.info("==> Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+    }
+});
